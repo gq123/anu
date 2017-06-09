57941ee4a2e8260dce26ab4f4de4fd6746978fe1
qiang.guang <qiang.guang@qunar.com>
2017-06-09 11:28:06 +0800

    Sinonjs

--- test/spec2.js
+++ test/spec2.js
@@ -1,9 +1,13 @@
 
-
+/*
 import './modules/createElement.spec'
 import './modules/util.spec'
 import './modules/style.spec'
 
 import './modules/classInherit.spec'
 import './modules/shallow.spec'
-import './modules/browser.spec'
+import './modules/browser.spec'*/
+//console.log(1111)
+//import './modules/test.spec'
+import './browser/context'
+import './browser/components'
